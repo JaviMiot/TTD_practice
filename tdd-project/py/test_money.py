91d74a00d13d7b0327c439df79ff53677c68e920
JaviMiot <51manobandajavier@gmail.com>
2022-05-27 09:05:00 -0500

    feat: divisionj and multiplication features done

--- tdd-project/py/test_money.py
+++ tdd-project/py/test_money.py
@@ -1,19 +1,38 @@
 import unittest
 
 
-class Dollar:
-    def __init__(self, amount: int):
+class Money:
+    def __init__(self, amount: int, currency: str):
         self.amount = amount
+        self.currency = currency
 
     def times(self, multiplier: int):
-        return Dollar(self.amount * multiplier)
+        return Money(self.amount * multiplier, self.currency)
+
+    def divide(self, divider: int):
+        return Money(self.amount / divider, self.currency)
+
+    def __eq__(self, other: object) -> bool:
+        return self.amount == other.amount and self.currency == other.currency
 
 
 class TestMoney(unittest.TestCase):
-    def testMultiplication(self):
-        fiver = Dollar(5)
-        tenner = fiver.times(2)
-        self.assertEqual(tenner.amount, 10)
+    def testMultiplicationDollars(self):
+        fiveDollars = Money(5, 'USD')
+        tenDollars = Money(10, 'USD')
+        self.assertEqual(fiveDollars.times(2), tenDollars)
+
+    def testMultiplicationEuro(self):
+        fiveEuros = Money(5, 'EUR')
+        tenEuros = Money(10, 'EUR')
+        self.assertEqual(fiveEuros.times(2), tenEuros)
+
+    def testDivision(self):
+        originalMoney = Money(4002, 'KRW')
+        actualMoneyAfterDivision = originalMoney.divide(4)
+        expectedMoneyAfterDivision = Money(1000.5, 'KRW')
+
+        self.assertEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision)
 
 
 if __name__ == '__main__':
