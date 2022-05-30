4241d25def2ac4c84861153ac40542151a9dc12d
JaviMiot <51manobandajavier@gmail.com>
2022-05-30 12:21:05 -0500

    doc: change folder name to practice

---
+++ TDD_practice/py/portfolio.py
@@ -0,0 +1,29 @@
+from functools import reduce
+
+from money import Money
+from bank import Bank
+
+
+class Portfolio:
+    def __init__(self):
+        self.moneys = []
+        self.exchange_rates = {'EUR->USD': 1.2, 'USD->KRW': 1100}
+
+    def add(self, *money):
+        self.moneys.extend(money)
+
+    def evaluate(self, bank: Bank, currency: str) -> Money:
+        total = 0.0
+        failures = []
+
+        for m in self.moneys:
+            try:
+                total += bank.convert(m, currency).amount
+            except KeyError as ke:
+                failures.append(ke)
+
+        if len(failures) == 0:
+            return Money(total, currency)
+
+        failureMessage = ','.join(f.args[0] for f in failures)
+        raise Exception(f'Missing exchange rate(s):[{failureMessage}]')
